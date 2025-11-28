a6e891e39c81d433161d21027b6edf73572bb057
Miguel Angel GH <maickthe@gmail.com>
2025-11-28 10:23:08 -0600

    Update | Agrege comentarios y una funcion que permite leer e identificar los numeros

--- compilador/lexer.py
+++ compilador/lexer.py
@@ -8,20 +8,26 @@ class Token:
         return f"Token({self.tipo}, {repr(self.valor)})"
 
 class Lexer:
+    #Es un diccionario que establece las palabras reservadas del sistema
     PALABRAS_RESERVADAS = {
         'mostrar': 'MOSTRAR',
         'decir': 'DECIR',
+        'numero': 'NUMERO'
     }
 
+    #Constructor del lexer
     def __init__(self, texto):
         self.texto = texto
         self.pos = 0
         self.linea_actual = 1
         self.caracter_actual = self.texto[self.pos] if self.texto else None
 
+    #Manejo de errores
     def error(self):
         raise Exception(f'Carácter inválido en línea {self.linea_actual}')
 
+    #Avanzar un carácter (Mueve el “cursor” una posición a la derecha.
+    #Si ya no hay más caracteres (se pasó del final), pone caracter_actual = None, que significa fin del texto.)
     def avanzar(self):
         self.pos += 1
         if self.pos > len(self.texto) - 1:
@@ -29,10 +35,12 @@ class Lexer:
         else:
             self.caracter_actual = self.texto[self.pos]
 
+    #Saltar espacios en blanco
     def saltar_espacios(self):
         while self.caracter_actual is not None and self.caracter_actual.isspace():
             self.avanzar()
 
+    #Leer un string (texto entre comillas)
     def string(self):
         resultado = ''
         self.avanzar()
@@ -42,12 +50,21 @@ class Lexer:
         self.avanzar()
         return resultado
 
+    #Leer un identificador (o palabra reservada)
     def identificador(self):
         resultado = ''
         while self.caracter_actual is not None and (self.caracter_actual.isalnum() or self.caracter_actual == '_'):
             resultado += self.caracter_actual
             self.avanzar()
         return resultado
+    
+    #Leer un numero (Numeros enteros)
+    def numero(self):
+        resultado = ''
+        while self.caracter_actual is not None and self.caracter_actual.isdigit():
+            resultado += self.caracter_actual
+            self.avanzar()
+        return int(resultado)
 
     def obtener_siguiente_token(self):
         while self.caracter_actual is not None:
@@ -57,7 +74,11 @@ class Lexer:
 
             if self.caracter_actual == '"':
                 return Token('STRING', self.string(), self.linea_actual)
-
+            
+            if self.caracter_actual.isdigit():
+                valor = self.numero()
+                return Token('NUMERO', valor, self.linea_actual)
+            
             if self.caracter_actual.isalpha():
                 id_text = self.identificador()
                 if id_text in self.PALABRAS_RESERVADAS:
